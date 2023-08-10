17b0976ba985fae98e55e17ace95b5c576563009
JWSScott777 <jwsscott777@gmail.com>
2023-08-09 20:55:37 -0400

    Fourth Commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-//import { dark } from "@clerk/themes";
+import { dark } from "@clerk/themes";
 
 import "../globals.css";
 
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider
       appearance={{
-       // baseTheme: dark,
+        baseTheme: dark,
       }}
     >
       <html lang='en'>
